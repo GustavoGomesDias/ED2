3355c18d9e1e4ae22f41fe81f8e99e11a77eb43d
GustavoGomesDias <treeelevengg@gmail.com>
2021-01-31 19:17:56 -0300

    Lista 7

--- Trab1-GustavoGomesDias/main.c
+++ Trab1-GustavoGomesDias/main.c
@@ -3,14 +3,12 @@
 #include "cell.h"
 #include "list.h"
 
-int main(int argv, char **argc){
+int main(){
     int i;
     List *lst = create_list();
     for(i = 0; i < 10; i++){
-        lst = insert_first(lst, i);
+        insert_list(lst, i, (i + 1));
     }
-    print_list(lst);
-    free_list(lst);
     print_list(lst);
     return 0;
 }
